0d8a6766a311067f37b41835ef271ec3f2c07239
Marcin Ryznar <mryznar@onet.pl>
2024-07-27 20:43:52 +0200

    py_AHTx0 - update setup.py

--- setup.py
+++ setup.py
@@ -25,12 +25,19 @@ test_deps = [
 
 version = _read_version()
 
+# Read the contents of your README file
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="py_AHTx0",
     version=version,
     author="Kattni Rembor, Marcin Ryznar",
     author_email="poczta321123@onet.pl",
     description="A library to drive a AHT10 or AHT20 temperature, humidity sensor over I2C",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/4meters/py_AHTx0",
     license="MIT",
     keywords=["raspberry pi", "orange pi", "banana pi", "rpi", "AHT10", "AHT20", "i2c", "temperature", "humidity", "smbus2"],
     packages=['py_AHTx0'],
@@ -60,6 +67,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ]
 )
